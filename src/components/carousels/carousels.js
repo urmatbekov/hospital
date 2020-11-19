5d9a172bf7f68316550fc38c1dcf6e58fe1cd996
mr-oroz <oroz7307@gmail.com>
2020-11-19 21:12:02 +0600

    Navbar

---
+++ src/components/carousels/carousels.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Carousel from "react-bootstrap/Carousel";
+
+
+class Carousels extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/image/slider/img-01.jpg"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/image/slider/img-01.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/image/slider/img-01.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
+}
+
+export default Carousels;
---
+++ src/components/header/header.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, Form, FormControl, Button} from "react-bootstrap";
+import "./header.css";
+
+class Header extends Component {
+    render() {
+        return (
+            <Navbar bg="dark" expand="lg">
+                <div className='container my-navbar'>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto ">
+                            <Nav.Link href="#link"><span className="home">HOME</span>
+                                <ul>
+                                    <li>HOME ON</li>
+                                    <li>HOME TWO</li>
+                                    <li>HOME THREE</li>
+                                </ul>
+                            </Nav.Link>
+                            <Nav.Link href="#link"><span className='my-link'>ABOUT US</span></Nav.Link>
+                            <Nav.Link href="#link"><span className='my-link'>TREATMENTS</span></Nav.Link>
+                            <Nav.Link href="#link"><span className='my-link'>BLOG</span></Nav.Link>
+                            <Nav.Link href="#link"><span className='my-link'>CONTACT</span></Nav.Link>
+                            <Nav.Link href="#link"><span className='my-link'>PAGES</span></Nav.Link>
+                        </Nav>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Search Your Queries" className="mr-sm-2"/>
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
