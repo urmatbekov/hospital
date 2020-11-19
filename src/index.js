5fb53e7bdc42f0660261f599460b31f8ffe84414
erlan <clash.00kgz>
2020-11-19 19:57:46 +0600

    few

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from "./app/app";
 
 ReactDOM.render(
-  <App />,
+  <App/>,
   document.getElementById('root')
 );
