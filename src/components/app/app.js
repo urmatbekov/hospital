33133afc50b1a202c70db23eae7a34be7a47bb24
erlan <clash.00kgz>
2020-11-19 21:20:13 +0600

    ew

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Best from "../best/best";
 
 class App extends Component {
     render() {
         return (
             <div>
-                
+         <Best/>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./app/app";
+import App from "./components/app/app";
 
 ReactDOM.render(
   <App/>,
