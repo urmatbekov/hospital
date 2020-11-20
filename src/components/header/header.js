8612a1b14548463778a79ffd8775e0ff43bdb3c8
Your Name <you@example.com>
2020-11-21 00:52:59 +0600

    first commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,13 +9,51 @@ class Header extends Component {
                 <div className='container my-navbar'>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto ">
-                            <Nav.Link href="#link"><span className="home">HOME</span></Nav.Link>
+                        <Nav className="mr-auto my-navbar">
+                            <Nav.Link href="#link">
+                                <span className="home">HOME</span>
+                                <ul className='my-ul'>
+                                    <li>HOME ON</li>
+                                    <li>HOME TWO</li>
+                                    <li>HOME THREE</li>
+                                </ul>
+                            </Nav.Link>
                             <Nav.Link href="#link"><span className='my-link'>ABOUT US</span></Nav.Link>
-                            <Nav.Link href="#link"><span className='my-link'>TREATMENTS</span></Nav.Link>
-                            <Nav.Link href="#link"><span className='my-link'>BLOG</span></Nav.Link>
-                            <Nav.Link href="#link"><span className='my-link'>CONTACT</span></Nav.Link>
-                            <Nav.Link href="#link"><span className='my-link'>PAGES</span></Nav.Link>
+                            <Nav.Link href="#link">
+                                <span className='my-link'>TREATMENTS</span>
+                                <ul className='my-ul'>
+                                    <li>TREATMENT V1</li>
+                                    <li>TREATMENT V2</li>
+                                    <li>TREATMENT FULLWIDTH</li>
+                                    <li>TREATMENT MASONRY</li>
+                                    <li>TREATMENT DETAIL</li>
+                                </ul>
+                            </Nav.Link>
+                            <Nav.Link href="#link">
+                                <span className='my-link'>BLOG</span>
+                                <ul className='my-ul'>
+                                    <li>BLOG V1</li>
+                                    <li>BLOG V2</li>
+                                    <li>BLOG DETAIL</li>
+                                </ul>
+                            </Nav.Link>
+                            <Nav.Link href="#link">
+                                <span className='my-link'>CONTACT</span>
+                                <ul className='my-ul'>
+                                    <li>CONTACTUS</li>
+                                    <li>CONTACTUS2</li>
+                                </ul>
+                            </Nav.Link>
+                            <Nav.Link href="#link">
+                                <span className='my-link'>PAGES</span>
+                                <ul className='my-ul'>
+                                    <li>SERVICES</li>
+                                    <li>DEPARTMENTS</li>
+                                    <li>APPOINTMENT</li>
+                                    <li>404 PAGE</li>
+                                    <li>COMING SOON</li>
+                                </ul>
+                            </Nav.Link>
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search Your Queries" className="mr-sm-2"/>
