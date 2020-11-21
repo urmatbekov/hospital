de29df7ad7fc71e5fe888938d0079cf7702343ef
Mikki <kadyraliev05>
2020-11-21 21:15:47 +0600

    feef

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
-
 import Best from "../best/best";
-
 import Header from "../header/header";
 import Carousel from "../carousels/carousels";
+import Nav from "../nav/nav";
 
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Nav/>
                 <Header/>
                 <Carousel/>
                 <Best/>
