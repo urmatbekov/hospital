284b6b0a7733304fbefab9904bd5f60eccd02001
Your Name <you@example.com>
2020-11-24 01:24:36 +0600

    hover

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-
 import Best from "../best/best";
-
 import Header from "../header/header";
 import Swiper from "../carousels/carousels";
+// import Parallax from "../parallax/parallax";
+
+
 
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
                 <Header/>
                 <Swiper/>
                 <Best/>
+                {/*<Parallax/>*/}
             </div>
         );
     }
---
+++ src/components/parallax/parallax.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Parallax} from "react-scroll-parallax";
+
+class Parallax extends Component {
+    render() {
+        return (
+            <Parallax bgImage={"/public/image/parallax/bgparallax-01.jpg"}>
+                <div className="container">
+                    <div className="row">
+
+                    </div>
+                </div>
+            </Parallax>
+        );
+    }
+}
+
+export default Parallax;
