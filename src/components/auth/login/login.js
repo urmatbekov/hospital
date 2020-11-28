1a106c9a4d8b1fee9a41bdf0d2e4a14a1320bd12
erlan <clash.00kgz>
2020-11-28 18:55:47 +0600

    rrrrrrrrrr

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 import Service from "../../service";
 
 const initialState = {
-    username: "",
-    password: ""
+    email: "",
+    password: "",
 }
 
+
 class Login extends Component {
+
     service = new Service()
+
     state = initialState
+
 
     onChange = (e) => {
         const elem = e.currentTarget
@@ -16,9 +20,15 @@ class Login extends Component {
             [elem.name]: elem.value
         })
     }
+
     onSubmit = (e) => {
         e.preventDefault()
-        this.service.createLogin(this.state).then((data) => {
+        const state = this.state
+        if (state.password !== state.password2) {
+            this.setState({error: "Password not same"})
+            return;
+        }
+        this.service.createUser(this.state).then((data) => {
                 console.log(data)
                 this.setState(initialState)
             }
@@ -34,22 +44,25 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="col-md-4 offset-md-4 mt-4">
-                {this.state.error && <p>{this.state.error}</p>}
-
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="UserName">Login</label>
-                        <input onChange={this.onChange} className="form-control" name="username" type="text"
-                               id="UserName"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="Password">Password</label>
-                        <input onChange={this.onChange} className="form-control" name="password" type="password"
-                               id="Password"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Войти</button>
-                </form>
+            <div>
+                <div className="col-md-4 offset-md-4 mt-4">
+                    {this.state.error && <p>{this.state.error}</p>}
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="Email">Логин</label>
+                            <input onChange={this.onChange} className="form-control" name="email" type="email"
+                                   id="Email"
+                                   value={this.state.email}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="Password">Пароль</label>
+                            <input onChange={this.onChange} className="form-control" name="password" type="password"
+                                   id="Password"
+                                   value={this.state.password}/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Войти</button>
+                    </form>
+                </div>
             </div>
         );
     }
